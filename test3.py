70da439603ba4def5a78bed4910f88468af0942a
jaime echegaray <jechegarayz@gmail.com>
2014-04-07 10:25:39 -0400

    arreglo2

--- test3.py
+++ test3.py
@@ -11,4 +11,8 @@ for line in salida.readlines():
 index2=[]
 index2=sorted(enumerate(index), key=operator.itemgetter(1), reverse=True)
 
-print index2
+index3=[]
+for i in range(0,99):
+	index3.append(index2[i][0])
+
+print index3
